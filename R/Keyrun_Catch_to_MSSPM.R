058216b3d330272bb3a895006d1a948d874da645
Ron <ronald.klasky@noaa.gov>
2022-01-31 15:06:50 -0500

    First commit of conversion tools

---
+++ R/Keyrun_Catch_to_MSSPM.R
@@ -0,0 +1,57 @@
+#
+# Keyrun_Catch_to_MSSPM ######################################################
+#
+#
+## Brief Description ###########################################################
+# Script to convert Keyrun (i.e., Atlantis) sim index file to MSSPM catch file
+#
+#
+## Detailed Description ########################################################
+#
+# This function takes the input Keyrun (i.e., Atlantis) data frame, converts it
+# to an MSSPM formatted data frame, and writes it out to the passed
+# CSV file.
+#
+#
+## Example Usage ###############################################################
+#
+# inputDataFrame <- mskeyrun::simCatchIndex
+# startYear <- 55
+# outputFile <- '/home/rklasky/test/HarvestCatch_Keyrun.csv'
+# convertKeyrunCatchtoMSSPM(inputDataFrame,startYear,outputFile)
+
+## roxygen help ################################################################
+#' Keyrun to MSSPM Catch Conversion
+#'
+#' Conversion of Keyrun (i.e., Atlantis) Simulated Catch Index file to MSSPM Catch csv file
+#' @param inputDataFrame The Keyrun input simulated catch data frame
+#' @param startYear The year to start the conversion (earlier years will be skipped)
+#' @param outputfile The output csv data file containing the MSSPM catch formatted data
+#' @return n/a
+#' @examples
+#' remotes::install_github("NOAA-EDAB/ms-keyrun")
+#'
+#' inputDataFrame <- mskeyrun::simCatchIndex
+#' startYear <- 55
+#' outputFile <- '/home/rklasky/test/HarvestCatch_Keyrun.csv'
+#'
+#' convertKeyrunCatchtoMSSPM(inputDataFrame,startYear,outputFile)
+#'
+
+#' @export
+convertKeyrunCatchtoMSSPM <- function(inputDataFrame,startYear,outputFile) {
+
+  # Get only the catch rows (i.e., no cv) and years >= the startYear
+  simCatchData <- subset(inputDataFrame,year>=startYear & variable=='catch')
+
+  # Keep only the columns MSSPM will need
+  msspmColumns <- dplyr::select(simCatchData, year, Name, value)
+
+  # Convert rows into columns
+  msspmCatch <- tidyr::spread(msspmColumns, Name, value)
+
+  # Write out final table
+  readr::write_csv(msspmCatch,outputFile)
+
+}
+
---
+++ R/Keyrun_ObsBM_to_MSSPM.R
@@ -0,0 +1,66 @@
+#
+# Keyrun_ObsBM_to_MSSPM ######################################################
+#
+#
+## Brief Description ###########################################################
+# Script to convert Keyrun survey index file to MSSPM observed biomass file
+#
+#
+## Detailed Description ########################################################
+#
+# This function takes the input keyrun survey biomass data frame,
+# converts it to an MSSPM formatted data frame, and writes it out
+# to the passed CSV file.
+#
+#
+## Example Usage ###############################################################
+#
+# speciesMappingDataFrame <- mskeyrun::focalSpecies # needed to map SVSPP value to modelName
+# observedBMDataFrame <- mskeyrun::surveyIndexA4
+# surveySeason <- 'SPRING' # Use 'FALL' or "SPRING"
+# outputFile <- paste('/home/rklasky/test/BiomassAbsolute_',surveySeason,'.csv',sep="")
+# convertKeyrunObsBMtoMSSPM(speciesMappingDataFrame,observedBMDataFrame,surveySeason,outputFile)
+
+## roxygen help ################################################################
+#' Keyrun to MSSPM Biomass Conversion
+#'
+#' Conversion of Keyrun Survey Index file to MSSPM Observed Biomass csv file
+#' @param speciesMappingDataFrame The data frame that contains a mapping of the SVSPP numeric values to corresponding species names (i.e., modelName)
+#' @param observedBMDataFrame The input survey index (i.e. biomass) data frame
+#' @param surveySeason The survey season data to convert (valid options are currently FALL or SPRING)
+#' @param outputfile The output csv data file containing the MSSPM observed biomass formatted data
+#' @return n/a
+#' @examples
+#'
+#' remotes::install_github("NOAA-EDAB/ms-keyrun")
+#'
+#' speciesMappingDataFrame <- mskeyrun::focalSpecies # needed to map SVSPP value to modelName
+#' observedBMDataFrame <- mskeyrun::surveyIndexA4
+#' surveySeason <- 'SPRING' # Use 'FALL' or "SPRING"
+#' outputFile <- paste('/home/rklasky/test/BiomassAbsolute_',surveySeason,'.csv',sep="")
+#'
+#' convertKeyrunObsBMtoMSSPM(speciesMappingDataFrame,observedBMDataFrame,surveySeason,outputFile)
+#'
+#' @export
+convertKeyrunObsBMtoMSSPM <- function(speciesMappingDataFrame,observedBMDataFrame,surveySeason,outputFile) {
+
+  # Grab the rows for the desired season and data type using the desired survey index data frame
+  seasonData <- subset(observedBMDataFrame, SEASON==surveySeason & variable=='strat.biomass')
+
+  # Grab just the columns needed for MSSPM
+  seasonDataColumns <- as.data.frame(dplyr::select(seasonData, YEAR, SVSPP, value))
+  # Q1. Why do I need the as.data.frame here?
+
+  # Using the SVSPP-Name lookup data frame, replace the numeric SVSPP values with their modelName values
+  seasonDataColumns$SVSPP <- with(speciesMappingDataFrame, modelName[match(seasonDataColumns$SVSPP,SVSPP)])
+
+  # Rename the column header from SVSPP to Name
+  names(seasonDataColumns)[names(seasonDataColumns) == "SVSPP"] <- "Name"
+
+  # Create columns as species
+  msspmObsBM <- tidyr::spread(seasonDataColumns, Name, value)
+  # Q2. There is missing data, what to do? (i.e., Fall, SVSPP=32, 1979)
+
+  # Write out final table
+  readr::write_csv(msspmObsBM,outputFile)
+}
